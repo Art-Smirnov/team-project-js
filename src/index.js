161aa533b2acfbabcbf09d3e919cbc16cd2b1d91
Artem Smirnov <aratatem2013@gmail>
2021-05-08 18:31:23 +0300

    add default fetch logic

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import './shared_scss/main.scss';
 import './services/apiService.js';
 import './services/countriesCodes.js';
 import preloaderFactory from './services/placeholder/placeholder.js';
+import ApiService from './services/apiService.js';
 
 const preloader = preloaderFactory('.lds-roller');
-
+const apiService = new ApiService();
 // preloader.show();
+
+apiService.fetchDefaultEvents();
---
+++ src/services/apiService.js
@@ -0,0 +1,24 @@
+const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/';
+
+const MY_KEY = '0E9J50fYPIbPJoVb72f3QU7X5EGa9oFk';
+
+const DEFAULT_COUNTRY = 'US';
+export default class ApiService {
+  constructor() {
+    this.page = 1;
+  }
+
+  async fetchDefaultEvents() {
+    const response = await fetch(
+      `${BASE_URL}events.json?countryCode=${DEFAULT_COUNTRY}&apikey=${MY_KEY}`,
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const res = await response.json();
+
+    console.log(res._embedded.events);
+    return await Promise.resolve(res._embedded);
+  }
+}
