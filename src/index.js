87903b4c540431506a7b65b29793425898a40f4d
Ayta Arylova <ayta.arylova@gmail.com>
2021-05-08 18:27:08 +0300

    add hbs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import './shared_scss/main.scss';
 import './services/apiService.js';
 import './services/countriesCodes.js';
 import preloaderFactory from './services/placeholder/placeholder.js';
+import cardTmpl from './templates/card-list-item.hbs';
+import listTmpl from './templates/card-list.hbs';
 
 const preloader = preloaderFactory('.lds-roller');
 
