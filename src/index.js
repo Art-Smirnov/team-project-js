635353c3c63366806a2f34463eae34f613b87188
Artem Smirnov <aratatem2013@gmail>
2021-05-08 12:53:47 +0300

    add some js files and placeholder

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './scss/main.scss';
+import './services/apiService.js';
+import './services/countriesCodes.js';
+import preloaderFactory from './services/placeholder.js';
+
+const preloader = preloaderFactory('.lds-roller');
+
+preloader.show();
