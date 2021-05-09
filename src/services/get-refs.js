98c96a427cc8810297e1a7da57caa7bc381ad837
Irina Selina <iras198308@gmail.com>
2021-05-09 17:14:04 +0300

    add modal window

--- src/services/get-refs.js
+++ src/services/get-refs.js
@@ -1,5 +1,7 @@
 export default function getRefs() {
   return {
     cardList: document.querySelector('.card-list'),
+    overlay: document.querySelector('.overlay'),
+    overlayModal: document.querySelector('overlay-modal')
   };
 }
