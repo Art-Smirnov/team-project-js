92a26672888e83664dcc7aa15a28e598c73868b7
Irina Selina <iras198308@gmail.com>
2021-05-10 16:03:04 +0300

    добавляет стили модалки

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -16,26 +16,22 @@ async function onClickCard(e) {
   if (e.target.nodeName === 'IMG' || e.target.nodeName === 'DIV') {
     currentID = e.target.parentElement.dataset.id;
   }
-
   if (e.target.nodeName === 'H3' || e.target.nodeName === 'P') {
+    currentID = e.target.parentElement.parentElement.dataset.id;
+  }
+  if (e.target.nodeName === 'LI') {
+    currentID = e.target.dataset.id;
+  }
 
-    if (e.target.nodeName === 'H3' || e.target.nodeName === 'P') {
+  const result = await apiService.fetchDefaultEvents();
 
-      currentID = e.target.parentElement.parentElement.dataset.id;
-    }
-    if (e.target.nodeName === 'LI') {
-      currentID = e.target.dataset.id;
-    }
-
-    const result = await apiService.fetchDefaultEvents();
-
-    for (const el of result) {
-      if (el.id === currentID) {
-        getRefs().modal.insertAdjacentHTML('beforeend', modalTmpl(el));
-      }
+  for (const el of result) {
+    if (el.id === currentID) {
+      getRefs().modal.insertAdjacentHTML('beforeend', modalTmpl(el));
     }
   }
 }
+
 function onCloseModal(e) {
   console.dir(e.target);
   if (e.target.nodeName !== 'BUTTON' && e.target.className !== 'backdrop') {
