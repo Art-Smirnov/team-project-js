b9a5030b151e6358e3b18f5f8d748082fa0b99ad
Irina Selina <iras198308@gmail.com>
2021-05-11 21:30:01 +0300

    делает адаптивную модалку

--- src/components/search-more/search-more.js
+++ src/components/search-more/search-more.js
@@ -2,10 +2,10 @@ import refs from '../../services/get-refs';
 import ApiService from '../../services/apiService.js';
 
 const apiService = new ApiService();
-console.log(refs().moreButton);
+// console.log(refs().moreButton);
 refs().moreButton.addEventListener('click', onSearchMore);
 
 async function onSearchMore() {
    const result = await apiService.fetchDefaultEvents();
-    console.log(result);
+    // console.log(result);
 }
