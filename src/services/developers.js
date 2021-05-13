be13f0062c44e29aa5100d6b08b721ee3c43915d
Anna Myzika <myzikaanya@gmail.com>
2021-05-13 22:54:57 +0300

    add hbs

---
+++ src/services/developers.js
@@ -0,0 +1,47 @@
+export default [
+    {
+        nameDev: Anna Myzika,
+        image: '',
+        github: https://github.com/AnnaMyzika,
+    },
+    {
+        nameDev: Artem Smirnov,
+        image: '',
+        github: https://github.com/Art-Smirnov,
+    },
+    {
+        nameDev: Nika Nyrkova,
+        image: '',
+        github: https://github.com/NikaNyrkova,
+    },
+    {
+        nameDev: Dmitriy Hordyna,
+        image: '',
+        github: https://github.com/DmitryHordyna,
+    },
+    {
+        nameDev: Oleksii Bohatyrenko,
+        image: '',
+        github: https://github.com/Bohatyrenko,
+    },
+    {
+        nameDev: Lesya Stulkivska,
+        image: '',
+        github: https://github.com/Legrito,
+    },
+    {
+        nameDev: Andrey Cherniak,
+        image: '',
+        github: https://github.com/ChristianRC007,
+    },
+    {
+        nameDev: Ayta Arylova,
+        image: '',
+        github: https://github.com/AytaAA,
+    },
+    {
+        nameDev: Irina Selina,
+        image: '',
+        github: https://github.com/IrinaSelina,
+    },
+];
