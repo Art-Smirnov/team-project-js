c19df280dc6a81fdbf53bbec01bb78ac4c3bc849
Lesya Stulkivska <67697045+Legrito@users.noreply.github.com>
2021-05-14 13:03:54 +0300

    Fix hover on images

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ import getRefs from './services/get-refs.js';
 import cardTmpl from './templates/card-list-item.hbs';
 import renderSelectCountry from './components/search-form/renderSearchForm.js';
 import gameMarkup from './components/tic-tac-toe/game-markup.js';
-console.log(gameMarkup());
 // import modalWindow from './components/modal/modal.js';
 const preloader = preloaderFactory('.lds-roller');
 const refs = getRefs();
