c80599ba2ab582049a542690d76b14a83065ece5
Anna Myzika <myzikaanya@gmail.com>
2021-05-17 22:30:13 +0300

    add team

--- src/services/developers.js
+++ src/services/developers.js
@@ -1,47 +1,57 @@
 export default [
     {
+        nameDev: Andrey Cherniak,
+        image: '',
+        github: https://github.com/ChristianRC007,
+        linkedin: ,
+    },
+    {
         nameDev: Anna Myzika,
         image: '',
         github: https://github.com/AnnaMyzika,
+        linkedin: ,
     },
     {
         nameDev: Artem Smirnov,
         image: '',
         github: https://github.com/Art-Smirnov,
+        linkedin: ,
+    },
+     {
+        nameDev: Ayta Arylova,
+        image: '',
+         github: https://github.com/AytaAA,
+         linkedin: https://www.linkedin.com/in/ayta-arylova-45406470/,
+    },
+      {
+        nameDev: Dmitriy Hordyna,
+        image: '',
+          github: https://github.com/DmitryHordyna,
+          linkedin: ,
+    },
+       {
+        nameDev: Irina Selina,
+        image: '',
+           github: https://github.com/IrinaSelina,
+           linkedin: ,
+    },
+        {
+        nameDev: Lesya Stulkivska,
+        image: '',
+            github: https://github.com/Legrito,
+            linkedin: ,
     },
     {
         nameDev: Nika Nyrkova,
         image: '',
         github: https://github.com/NikaNyrkova,
+        linkedin: ,
     },
-    {
-        nameDev: Dmitriy Hordyna,
-        image: '',
-        github: https://github.com/DmitryHordyna,
-    },
+   
     {
         nameDev: Oleksii Bohatyrenko,
         image: '',
         github: https://github.com/Bohatyrenko,
-    },
-    {
-        nameDev: Lesya Stulkivska,
-        image: '',
-        github: https://github.com/Legrito,
-    },
-    {
-        nameDev: Andrey Cherniak,
-        image: '',
-        github: https://github.com/ChristianRC007,
-    },
-    {
-        nameDev: Ayta Arylova,
-        image: '',
-        github: https://github.com/AytaAA,
-    },
-    {
-        nameDev: Irina Selina,
-        image: '',
-        github: https://github.com/IrinaSelina,
+        linkedin: ,
     },
 ];
